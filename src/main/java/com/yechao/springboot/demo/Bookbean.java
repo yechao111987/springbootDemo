dde2ca711e66704ac1de4217f960fbdaef5bcf11
yechao111987 <yechao111987@126.com>
2017-12-29 13:39:40 +0800

    springbootDemo

---
+++ src/main/java/com/yechao/springboot/demo/Bookbean.java
@@ -0,0 +1,38 @@
+package com.yechao.springboot.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "book")
+@PropertySource(value = "classpath:book.properties")
+public class Bookbean {
+    private String name;
+    private String author;
+    private String price;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+}
