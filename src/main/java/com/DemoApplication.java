01eae41b51644042bfcfce766394b48e907ff31e
yechao <yechao111987@126.com>
2018-02-26 19:41:54 +0800

    新增静态模块，配置idea热部署，新增thymleaf

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @SpringBootApplication   //整个Spring Boot的核心注解，它的目的就是开启Spring Boot的自动配置
-@RestController   //使之变为一个Controller，然后里边提供一个地址转换方法
+//@RestController   //使之变为一个Controller，然后里边提供一个地址转换方法
+@Controller
 public class DemoApplication {
 
     @Value("${config.name}")
@@ -29,7 +32,7 @@ public class DemoApplication {
     private Bookbean bookbean;
 
     @Autowired
-    private  UserDao userDao;
+    private UserDao userDao;
 
     private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
@@ -39,10 +42,17 @@ public class DemoApplication {
         new SpringApplicationBuilder(DemoApplication.class).web(true).run(args);
     }
 
-    @RequestMapping(value = "/", produces = "text/plain;charset=UTF-8")
-    String index() {
+    @RequestMapping(value = "/test", produces = "text/plain;charset=UTF-8")
+    String indextest() {
         String str = "My Name is " + appname + "," + "age is " + age + " !";
         return str;
+    }
+
+    @RequestMapping(value = "/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "121212");
+        String str = "My Name is " + appname + "," + "age is " + age + " !";
+        return "test1";
     }
 
     @RequestMapping(value = "/book", produces = "text/plain;charset=UTF-8")
