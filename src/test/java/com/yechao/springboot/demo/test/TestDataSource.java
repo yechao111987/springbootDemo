11dea9dc41474098c23459d2b9562d2e919d859c
Ye Chao <yechao111987@126.com>
2018-10-15 14:10:17 +0800

    解决编译报错:package javafx.application does not exist

--- src/test/java/com/yechao/springboot/demo/test/TestDataSource.java
+++ src/test/java/com/yechao/springboot/demo/test/TestDataSource.java
@@ -1,6 +1,5 @@
 package com.yechao.springboot.demo.test;
 
-import javafx.application.Application;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
