2eeca02226f37a0921869edd18733b0b3b4dc08c
yechao <yechao111987@126.com>
2018-12-29 11:33:16 +0800

    fix safe pdf

--- src/main/scala/com/TestScalaList.scala
+++ src/main/scala/com/TestScalaList.scala
@@ -1,11 +1,10 @@
-
 package com
+
 
 import org.junit.{Assert, Test}
 
 class TestScalaList {
   def main(args: Array[String]): Unit = {
-
     println("hello,Main")
   }
 
@@ -14,13 +13,24 @@ class TestScalaList {
   }
 }
 
+object TestScalaMain {
+  def main(args: Array[String]): Unit = {
+    println("hello,Main")
+  }
+
+  def count(a: Int, b: Int): Int = {
+    return a + b
+  }
+}
+
+
 @Test
 class DemoTest extends Assert {
   @Test
   def test1: Unit = {
     var a: Int = 5
     var b: Int = 6
-    var c: Int = 12
+    var c: Int = 11
     val test1 = new TestScalaList
     var c1: Int = test1.count(a, b)
     assert(c1 == c, "测试不通过")
